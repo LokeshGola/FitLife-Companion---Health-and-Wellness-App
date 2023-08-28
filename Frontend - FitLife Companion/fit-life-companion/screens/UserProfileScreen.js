8be65770a6dfed51c906105060361fb2d8bef673
LokeshGola <lokeshgola121@gmail.com>
2023-08-28 14:23:34 +0530

    milestone 1

---
+++ Frontend - FitLife Companion/fit-life-companion/screens/UserProfileScreen.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+
+import { StyleSheet } from 'react-native';
+
+
+const UserProfileScreen = () => {
+  const [name, setName] = useState('');
+  const [age, setAge] = useState('');
+  const [gender, setGender] = useState('');
+
+  const handleSaveProfile = () => {
+    // Logic to save the profile details
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text>User Profile Screen</Text>
+      <TextInput style={styles.input}
+        placeholder="Name"
+        value={name}
+        onChangeText={setName}
+      />
+      <TextInput style={styles.input}
+        placeholder="Age"
+        value={age}
+        onChangeText={setAge}
+      />
+      <TextInput style={styles.input}
+        placeholder="Gender"
+        value={gender}
+        onChangeText={setGender}
+      />
+      <Button title="Save" onPress={handleSaveProfile} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    input: {
+      width: '80%',
+      height: 40,
+      borderWidth: 1,
+      marginBottom: 10,
+      paddingHorizontal: 10,
+    },
+  });
+
+export default UserProfileScreen;
